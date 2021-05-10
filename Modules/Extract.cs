cf891426378de4094ca219a250ba36e0cd503bc9
ZamarTerrier <ZamarTerrier@yandex.ru>
2021-05-10 23:57:01 +0500

    Set Hunt Timber and Harvest

---
+++ Modules/Extract.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Zamargrad.Logic.Extract;
+
+namespace Zamargrad.Modules
+{
+    [Name("Extract")]
+    public class Extract : ModuleBase<SocketCommandContext>
+    {
+        [Command("добывать")]
+        [Summary("Добыть руду")]
+        public Task Harvest() => ExtractService.Harvest(Context);
+
+        [Command("рубить")]
+        [Summary("Рубить дерево")]
+        public Task Timber() => ExtractService.Timber(Context);
+
+        [Command("охота")]
+        [Summary("Пойти на охоту")]
+        public Task Hunt() => ExtractService.Hunt(Context);
+
+    }
+}
